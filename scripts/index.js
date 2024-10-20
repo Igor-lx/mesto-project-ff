ae71479a18d90fa7b4482ff7b2ae9b2dde220ba5
Igor B <lorax.k@yandex.ru>
2024-10-20 16:10:21 +0200

    v0.1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,16 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+/*  const initialCards - массив объектов карточек
+    initialCard.name - имя карточки
+    initialCards.ink - ссылка
+*/
+
+const cardPlaceNode = document.querySelector('.places__list'); 
+const cardItem = document.querySelector('#card-template').content.cloneNode(true);
+
+cardItem.querySelector('.card__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
+cardItem.querySelector('.card__title').textContent = 'Дюк Корморант';
+
+cardPlaceNode.append(cardItem);
