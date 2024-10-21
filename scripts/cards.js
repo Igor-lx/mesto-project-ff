0f76a380320ede6ff71cfdb65ae93c5fb4ca6c98
Igor B <lorax.k@yandex.ru>
2024-10-21 15:21:28 +0200

    v0.8

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
-    {
-      name: "мост через реку Тара, Черногория.",
-      link: "./images/CG1.jpg",
-    },
-    {
-      name: "Будва, старый город",
-      link: "./images/CG2.jpg",
-    },
-    {
-      name: "Бечичи",
-      link: "./images/CG3.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Бечичи",
+    link: "./images/CG1.jpg",
+  },
+  {
+    name: "Будва, старый город",
+    link: "./images/CG2.jpg",
+  },
+  {
+    name: "Петровац",
+    link: "./images/CG3.jpg",
+  },
+  {
+    name: "Мост через реку Тара ",
+    link: "./images/CG4.jpg",
+  },
+  {
+    name: "Котор",
+    link: "./images/CG5.jpg",
+  },
+  {
+    name: "Будванская балерина>",
+    link: "./images/CG6.jpg",
+  },
 ];
