58bc1756fbf0dec568b6ed52ef35e5ffa304b243
Igor B <lorax.k@yandex.ru>
2024-10-22 12:30:56 +0200

    v1.2++

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+
 function createCard(cardData, deleteCardFunction) {
   const cardItem = document
     .querySelector("#card-template")
@@ -6,31 +7,20 @@ function createCard(cardData, deleteCardFunction) {
   const cardItemImage = cardItem.querySelector(".card__image");
   const cardItemTitle = cardItem.querySelector(".card__title");
   const deleteCardButton = cardItem.querySelector(".card__delete-button");
-
-  /*--------------------------------------------------------------------------*/
-  const cardItemDescription = cardItem.querySelector(".card__description");
-  const cardLikeButton = cardItem.querySelector(".card__like-button");
-  /*--------------------------------------------------------------------------*/
+  
 
   cardItemImage.src = cardData.link;
   cardItemImage.alt = 'фотография: "' + cardData.name + '"';
   cardItemTitle.textContent = cardData.name;
 
-  /*--------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
+  const cardItemDescription = cardItem.querySelector(".card__description");
+  const cardLikeButton = cardItem.querySelector(".card__like-button");
+  
   cardItemImage.addEventListener("error", function () {
-    cardItemImage.classList.add(
-      "card__image__load_failure__textstile",
-      "card__image__load_failure"
-    );
-    cardItemTitle.classList.add("card__image__load_failure__textstile");
-    cardItemTitle.textContent =
-      "Упс! Изображение не найдено, но мы уже отправили за ним поисковую команду.";
-
-    cardItemDescription.classList.add("card__image__load_failure__description");
-    cardLikeButton.style.display = "none";
+    processImgDownldError(cardItemImage, cardItemTitle, cardItemDescription, cardLikeButton);
   });
-
-  /*--------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
 
   deleteCardButton.addEventListener("click", function (evt) {
     deleteCardFunction(evt.target.parentElement);
@@ -42,6 +32,21 @@ function createCard(cardData, deleteCardFunction) {
 function deleteCardFunction(card) {
   card.remove();
 }
+
+/* ------------------------------------------------------------------------------- */
+function processImgDownldError(cardItemImage, cardItemTitle, cardItemDescription, cardLikeButton) {
+  
+  cardItemImage.classList.add("card__image__load_failure__textstile", "card__image__load_failure");
+  cardItemTitle.classList.add("card__image__load_failure__textstile");
+  cardItemTitle.textContent = "Упс! Изображение не найдено, но мы уже отправили за ним поисковую команду.";
+  cardItemDescription.classList.add("card__image__load_failure__description");
+  cardLikeButton.style.display = "none";
+}
+/* ------------------------------------------------------------------------------- */
+
+
+
+
 const cardPlace = document.querySelector(".places__list");
 
 initialCards.forEach(function (index) {
