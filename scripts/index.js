7a04f4af37e44f18b0dfda2060a4bffcaa36a234
Igor B <lorax.k@yandex.ru>
2024-10-22 12:00:05 +0200

    v1.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-function addCard(cardData, deleteCardFunction) {
+function createCard(cardData, deleteCardFunction) {
   const cardItem = document
     .querySelector("#card-template")
     .content.cloneNode(true);
@@ -7,8 +7,30 @@ function addCard(cardData, deleteCardFunction) {
   const cardItemTitle = cardItem.querySelector(".card__title");
   const deleteCardButton = cardItem.querySelector(".card__delete-button");
 
+  /*--------------------------------------------------------------------------*/
+  const cardItemDescription = cardItem.querySelector(".card__description");
+  const cardLikeButton = cardItem.querySelector(".card__like-button");
+  /*--------------------------------------------------------------------------*/
+
   cardItemImage.src = cardData.link;
+  cardItemImage.alt = 'фотография: "' + cardData.name + '"';
   cardItemTitle.textContent = cardData.name;
+
+  /*--------------------------------------------------------------------------*/
+  cardItemImage.addEventListener("error", function () {
+    cardItemImage.classList.add(
+      "card__image__load_failure__textstile",
+      "card__image__load_failure"
+    );
+    cardItemTitle.classList.add("card__image__load_failure__textstile");
+    cardItemTitle.textContent =
+      "Упс! Изображение не найдено, но мы уже отправили за ним поисковую команду.";
+
+    cardItemDescription.classList.add("card__image__load_failure__description");
+    cardLikeButton.style.display = "none";
+  });
+
+  /*--------------------------------------------------------------------------*/
 
   deleteCardButton.addEventListener("click", function (evt) {
     deleteCardFunction(evt.target.parentElement);
@@ -20,8 +42,8 @@ function addCard(cardData, deleteCardFunction) {
 function deleteCardFunction(card) {
   card.remove();
 }
+const cardPlace = document.querySelector(".places__list");
 
 initialCards.forEach(function (index) {
-  const cardPlace = document.querySelector(".places__list");
-  cardPlace.append(addCard(index, deleteCardFunction));
+  cardPlace.append(createCard(index, deleteCardFunction));
 });
