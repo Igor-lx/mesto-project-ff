d37f21ff995a224b7491880e267eaba32b2efa3d
Igor B <lorax.k@yandex.ru>
2024-11-14 00:15:48 +0100

    v.1.7_image_fullscreen

--- src/scripts/card_create_initial.js
+++ src/scripts/card_create_initial.js
@@ -1,26 +1,37 @@
-function createCard(name, link, deleteCallback, errorCallback, likeCallback) {
+function createCard(
+  name,
+  link,
+  deleteCallback,
+  errorCallback,
+  likeCallback,
+  fullscreenCallback
+) {
   const nodeTemplate = document.querySelector("#card-template").content;
   const cardItem = nodeTemplate.querySelector(".card").cloneNode(true);
 
-  const cardItemImage = cardItem.querySelector(".card__image");
   const cardItemTitle = cardItem.querySelector(".card__title");
-  const deleteCardButton = cardItem.querySelector(".card__delete-button");
-  const cardLikeButton = cardItem.querySelector(".card__like-button");
+  const cardItemImage = cardItem.querySelector(".card__image");
 
   cardItemTitle.textContent = name;
   cardItemImage.src = link;
   // cardItemImage.setAttribute("src", link);  // а можно вот так
   cardItemImage.alt = 'фотография: "' + name + '"';
 
+  const deleteCardButton = cardItem.querySelector(".card__delete-button");
   deleteCardButton.addEventListener("click", function () {
     deleteCallback(cardItem);
   });
 
+  const cardLikeButton = cardItem.querySelector(".card__like-button");
   cardLikeButton.addEventListener("click", function (evt) {
     likeCallback(evt.target);
   });
 
-  /* -------------------------------------------------------------------- + выравнивание при ошибке загрузки img---- */
+  cardItemImage.addEventListener("click", function (evt) {
+    fullscreenCallback(evt.target);
+  });
+
+  /* ---------------------------------------------------------------------- + обработчик ошибки загрузки img ---- */
   const cardItemDescription = cardItem.querySelector(".card__description");
 
   cardItemImage.onerror = () => {
@@ -31,7 +42,7 @@ function createCard(name, link, deleteCallback, errorCallback, likeCallback) {
       cardLikeButton
     );
   };
-  /* --------------------------------------------------------------------------------------------------------------- */
+  /* ----------------------------------------------------------------------------------------------------------- */
 
   return cardItem;
 }
@@ -44,7 +55,17 @@ function likeCard(i) {
   i.classList.toggle("card__like-button_is-active");
 }
 
-/* ----------------------------------------------------------------------------------------------------------------- */
+function openFullscreenImage(i) {
+  const popupImage = document.querySelector(".popup__image");
+  const popupImageCaption = document.querySelector(".popup__caption");
+
+  popupImage.src = i.src;
+  popupImage.alt = i.alt;
+  popupImageCaption.textContent =
+    i.parentElement.querySelector(".card__title").textContent;
+}
+
+/* ------------------------------------------------------- + функция выравнивания при ошибке загрузки img ---- */
 function processImgDownldError(Image, Title, Description, Button) {
   Image.classList.add(
     "card__image__load_failure__textstile",
@@ -56,7 +77,7 @@ function processImgDownldError(Image, Title, Description, Button) {
   Description.classList.add("card__image__load_failure__description");
   Button.style.display = "none";
 }
-/* ---------------------------------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------------------------- */
 
 function renderCard(cards) {
   const cardPlace = document.querySelector(".places__list");
@@ -64,7 +85,14 @@ function renderCard(cards) {
   if (Array.isArray(cards)) {
     cards.forEach(function (i) {
       cardPlace.append(
-        createCard(i.name, i.link, deleteCard, processImgDownldError, likeCard)
+        createCard(
+          i.name,
+          i.link,
+          deleteCard,
+          processImgDownldError,
+          likeCard,
+          openFullscreenImage
+        )
       );
     });
   } else {
@@ -74,7 +102,8 @@ function renderCard(cards) {
         cards.link,
         deleteCard,
         processImgDownldError,
-        likeCard
+        likeCard,
+        openFullscreenImage
       )
     );
   }
