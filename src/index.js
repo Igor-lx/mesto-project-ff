ee5ed5b2c8181a28a2122d24cc8913479fedc031
Igor B <lorax.k@yandex.ru>
2024-11-15 00:14:25 +0100

    fixes+reworked_profile_form

--- src/index.js
+++ src/index.js
@@ -6,41 +6,58 @@ import { initialCardsArray } from "./scripts/card_data";
 import { renderCard } from "./scripts/card_create_initial";
 import { openModal, closeModal } from "./scripts/modal_handler";
 
-//import { editProfile } from "./scripts/profile_management";
 //import { createCustomCard } from "./scripts/card_create_custom";
 
 /* ------------------------------------------------------------ ИСПОЛНЯЕМЫЙ JS ---- */
 
 renderCard(initialCardsArray);
 
-const modals = [
-  {
-    button: document.querySelector(".profile__edit-button"),
-    window: document.querySelector(".popup_type_edit"),
-  },
-  {
-    button: document.querySelector(".profile__add-button"),
-    window: document.querySelector(".popup_type_new-card"),
-  },
-];
+/* открытие модалок */
 
-const cardImages = document.querySelectorAll(".card__image");
-const ImageModalWindow = document.querySelector(".popup_type_image");
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileModalWindow = document.querySelector(".popup_type_edit");
 
-modals.forEach((item) => {
-  item.button.addEventListener("click", () => openModal(item.window));
+const newcardAddButton = document.querySelector(".profile__add-button");
+const newcardModalWindow = document.querySelector(".popup_type_new-card");
+
+const cardImage = document.querySelectorAll(".card__image");
+const cardImageModalWindow = document.querySelector(".popup_type_image");
+
+profileEditButton.addEventListener("click", () => {
+  nameInputField.value = profileName.textContent;
+  jobInputField.value = profileJob.textContent;
+  openModal(profileModalWindow);
 });
 
-cardImages.forEach((item) => {
-  item.addEventListener("click", () => openModal(ImageModalWindow));
+newcardAddButton.addEventListener("click", () => openModal(newcardModalWindow));
+
+cardImage.forEach((item) => {
+  item.addEventListener("click", () => openModal(cardImageModalWindow));
 });
+
+/* редактирование профиля */
+
+const ProfileFormElement = document.querySelector('[name="edit-profile"]');
+
+const profileName = document.querySelector(".profile__title");
+const profileJob = document.querySelector(".profile__description");
+
+const nameInputField = document.querySelector('[name="person_name"]');
+const jobInputField = document.querySelector('[name="description"]');
+
+ProfileFormElement.addEventListener("submit", (evt) => {
+  editProfile(evt);
+  closeModal(profileModalWindow);
+});
+
+function editProfile(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInputField.value;
+  profileJob.textContent = jobInputField.value;
+}
 
 /*
-const editProfileFormElement = document.querySelector('[name="edit-profile"]');
 const submitCardFormElement = document.querySelector('[name="new-place"]');
-
-editProfileFormElement.addEventListener("submit", editProfile);
-
 submitCardFormElement.addEventListener("submit", function (evt) {
   const newCardData = createCustomCard(evt);
   renderCard(newCardData);
@@ -49,7 +66,7 @@ submitCardFormElement.addEventListener("submit", function (evt) {
 */
 
 /* ------------------------------------------------------------------- ЭКСПОРТ ---- */
-export { importedImages, initialCardsArray, renderCard, closeModal };
+export { importedImages, initialCardsArray, renderCard };
 
 //
 //
